a6335aee787e87cc7543c537db955f646bda0823
Felix Zavala <63223411+JFELIXZAVALA@users.noreply.github.com>
2022-01-25 22:58:29 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+
+
+var database =[
+    {
+        username: "admin",
+        password: "password"
+    }
+]
+
+var newsFeed =[
+
+    {username:"Bobby",
+     timeline:"Yo!"
+    },
+    {
+        username:"Sally",
+        timeline:"JS is dope"
+    }
+    
+    
+    ];
+
+function validate(username, password)
+{
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    if (username === database[0].username && password === database[0].password)
+    {
+        alert("login succesfully"),
+        function validate() {
+            window.location.href="http://programminghead.com";  
+          }
+        
+        
+    }
+    else
+    {
+        alert("Incorrect Credentials");
+    }
+}
+
+validate(username, password);
+
+
+
+
+
+
