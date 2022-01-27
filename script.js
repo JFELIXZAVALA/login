33ff03799a247f66b3946faee1f67ba0e8d0f7c2
Felix Zavala <63223411+JFELIXZAVALA@users.noreply.github.com>
2022-01-26 23:44:32 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function auth() {
     var email = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
-    if (email === "admin" && password === "p") {
+    if (email === "admin" && password === "pass") {
         window.location.replace("http://www.felixzavala.com");
         alert("Login Successful");
 
